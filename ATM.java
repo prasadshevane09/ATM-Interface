cda96e9c0747442c3ebafdc00bc3a28159ffc9b3
Prasad Gopal Shevane <140835079+prasadshevane09@users.noreply.github.com>
2023-08-01 14:47:25 +0530

    ATM.java

--- ATM.java
+++ ATM.java
@@ -10,13 +10,16 @@ public class ATM {
         return balance;
     }
 
-    public void withdraw(double amount) {
-        if (amount > 0 && amount <= balance) {
-            balance -= amount;
+    public void withdraw(double withdraw_amount) {
+        if (withdraw_amount > 0 && withdraw_amount <= balance) {
+            balance -= withdraw_amount;
             System.out.println("Withdrawal successful. Remaining balance: " + balance);
         } else {
             System.out.println("Invalid withdrawal amount or insufficient balance.");
         }
+    }
+    public void deposit(double deposit_amount) {
+        balance += deposit_amount;
     }
 
     public static void main(String[] args) {
@@ -30,8 +33,9 @@ public class ATM {
             System.out.println("\nWelcome to the ATM!");
             System.out.println("1. Check Balance");
             System.out.println("2. Withdraw");
-            System.out.println("3. Exit");
-            System.out.print("Enter your choice (1/2/3): ");
+            System.out.println("3. Deposit");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice (1/2/3/4): ");
             int choice = scanner.nextInt();
 
             switch (choice) {
@@ -40,10 +44,15 @@ public class ATM {
                     break;
                 case 2:
                     System.out.print("Enter the amount to withdraw: ");
-                    double amount = scanner.nextDouble();
-                    atm.withdraw(amount);
+                    double withdraw_amount = scanner.nextDouble();
+                    atm.withdraw(withdraw_amount);
                     break;
                 case 3:
+                    System.out.print("Enter the amount to deposit");
+                    double deposit_amount = scanner.nextDouble();
+                    atm.deposit(deposit_amount);
+                    break;
+                case 4:
                     System.out.println("Thank you for using the ATM. Goodbye!");
                     scanner.close();
                     System.exit(0);
